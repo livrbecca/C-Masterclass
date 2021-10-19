6d8a0daecbbd055d794f3b76d2866123d11a9843
livrbecca <69110329+livrbecca@users.noreply.github.com>
2021-10-19 12:56:52 +0100

    added user login project

---
+++ UserLogin/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace UserLogin
+{
+    class Program
+    {
+        // created globally
+        static string username;
+        static string password;
+        static void Main(string[] args)
+        {
+            Register();
+            Login();
+            Console.Read();
+        }
+
+
+        public static void Register()
+        { // static so we can use it in main
+            Console.WriteLine("enter username");
+            username = Console.ReadLine();
+            Console.WriteLine("enter password");
+            password = Console.ReadLine(); Console.WriteLine("registration complete\n ___________________________");
+        }
+
+        public static void Login()
+        { // static so we can use it in main
+            Console.WriteLine("enter username");
+            if (username == Console.ReadLine())
+            {
+                Console.WriteLine("enter password");
+                if (password == Console.ReadLine())
+                {
+                    Console.WriteLine("login successful");
+                }
+                else
+                {
+                    Console.WriteLine("invalid password, try again");
+                }
+            }
+            else
+            {
+                Console.WriteLine("invalid username, try again");
+            }
+
+
+        }
+    }
+
+}
