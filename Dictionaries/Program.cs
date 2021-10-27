7a693940b4f24b10c654dc1641538a730c0527d5
Olivia <69110329+livrbecca@users.noreply.github.com>
2021-10-27 17:06:48 +0100

    -

---
+++ Dictionaries/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionaries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // TKey, TValue = type of each
+            Dictionary<int, string> myDictionary = new Dictionary<int, string>();
+        }
+    }
+}
